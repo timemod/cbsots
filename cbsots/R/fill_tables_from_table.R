bc936da85b6a68b18bd4a5f49e1db0759431c75b
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2018-07-05 14:50:50 +0200

    Corrected errors in function fill_tables_from_table. Added a testjob.

--- cbsots/R/fill_tables_from_table.R
+++ cbsots/R/fill_tables_from_table.R
@@ -1,32 +1,35 @@
 #' Fill tables with values from another table.
 #' 
+#' This function fills in the Select and Code fields from a base table based
+#' on common Keys or Titles.
+#' 
 #' @param tscodes a \code{ts_code} object. This object can be created
 #' and modified with function \code{\link{edit_ts_code}}, which starts a Shiny
 #' app
-#' @param ids ids of tables to be filled from the second tabel
-#' @param basis_id id of the table whose values will be used to fill
-#' the first tables.
+#' @param ids ids of tables to be filled from the base tabel
+#' @param base_id the id of the base table
 #' @param base_url optionally specify a different server. Useful for third party
 #' data services implementing the same protocol.
 #' @export
-fill_tables_from_table <- function(tscodes, ids,  basis_id, base_url = NULL) {
+fill_tables_from_table <- function(tscodes, ids,  base_id, base_url = NULL) {
   
-  basis_table <- tscodes$table_code[[basis_id]]$codes
+  base_table <- tscodes$table_code[[base_id]]
   
   new_table_created <- FALSE
   
   for (id in  ids) {
     
     if (id %in% names(tscodes$table_code)) {
-      table <- tscodes$table_code[[id]]$codes
+      table <- tscodes$table_code[[id]]
     } else {
       table <- create_new_table(id, base_url)
       new_table_created <- TRUE
     }
     
-    table <- fill_table_from_table(table, basis_table)
+    table <- fill_table_from_table(table, base_table)
   
-    tscodes$table_code[[id]]$codes <- table
+    tscodes$table_code[[id]] <- table
+    
   }
   
   if (new_table_created) {
@@ -36,24 +39,45 @@ fill_tables_from_table <- function(tscodes, ids,  basis_id, base_url = NULL) {
   return(tscodes)
 }
 
-# internal function
-fill_table_from_table <- function(table, basis_table) {
+# internal function: fill in Select and Code based on common Keys or Titles
+fill_table_from_table <- function(table, base_table) {
   
-  fill_code_from_code <- function(name) {
-    code <- table[[name]]
-    basis_code <- basis_table[[name]]
+  fill_code_from_code <- function(dimension) {
     
-    common_keys <- intersect(code$Key, basis_code$Key)
+    code <- table$codes[[dimension]]
+    base_code <- base_table$codes[[dimension]]
+    
+    common_keys <- intersect(code$Key, base_code$Key)
     rows <- match(common_keys, code$Key)
-    basis_rows <- match(common_keys, basis_code$Key)
-    code[rows, "Select"] <- basis_code[basis_rows, "Select"]
-    code[rows, "Code"] <- basis_code[basis_rows, "Code"]
-    code[rows, "Title"] <- basis_code[basis_rows, "Title"]
+    base_rows <- match(common_keys, base_code$Key)
+    common_titles <- intersect(code$Title, base_code$Title)
+    rows <- union(rows, match(common_titles, code$Title))
+    base_rows <- union(base_rows, match(common_titles, base_code$Title))
+    
+    code[rows, "Select"] <- base_code[base_rows, "Select"]
+    code[rows, "Code"] <- base_code[base_rows, "Code"]
+    
+    # If the keys were ordered according to original cbs ordering in the base
+    # table, then they were not ordered in the origina table.
+    # then the keys were ordered according to selected first.
+    cbs_order <- identical(base_code$Key, base_code$OrigKeyorder)
+    if (!cbs_order) {
+      code <- order_code_rows(code, cbs_order)
+    }
+
     return(code)
   }
   
-  common_names <- intersect(names(table), names(basis_table))
-  new_codes <- sapply(common_names, FUN = fill_code_from_code, simplify = FALSE)
-  table <- modifyList(table, new_codes)
+  common_dimensions <- intersect(names(table$codes), names(base_table$codes))
+  new_codes <- sapply(common_dimensions, FUN = fill_code_from_code, 
+                      simplify = FALSE)
+  table$codes <- modifyList(table$codes, new_codes)
+  
+  # also adapt the ordering
+  ord <- match(base_table$order, table$order)
+  ord <- ord[!is.na(ord)]
+  ord <- c(ord, setdiff(seq_along(table$order), ord))
+  table$order <- table$order[ord]
+  
   return(table)
 }
---
+++ cbsots/tests/testthat/test_fill_table_1.R
@@ -0,0 +1,30 @@
+library(cbsots)
+library(testthat)
+
+rm(list = ls())
+
+context("fill_tables_from_table")
+
+ts_code_file_old <- "tscode/tscode_81974NED.rds"
+
+
+ts_code <- readRDS(ts_code_file_old)
+
+
+test_that("fill 83935NED from 81974NED", {
+  tscodes_new <- fill_tables_from_table(ts_code, ids = "83935NED", 
+                                        base_id = "81974NED")
+
+  expect_known_value(tscodes_new, "expected_output/full_table_1.rds")
+  
+  if (FALSE) {
+
+    print(tscodes_new$table_code$`83935NED`)
+
+    ts_code_file_tmp <- "tscode/tscode_83935NED_tmp.rds"
+    saveRDS(tscodes_new, ts_code_file_tmp)
+    edit_ts_code(ts_code_file = ts_code_file_tmp)
+
+    unlink(ts_code_file_tmp)
+  }
+})
