50b2ab9f9e1ae47b593e406356cc7af46722cb7b
Rob van Harrevelt <rvanharrevelt@gmail.com>
2020-11-12 09:37:57 +0100

    Fixed example test_get_ts.R

--- examples/test_get_ts.R
+++ examples/test_get_ts.R
@@ -1,13 +1,10 @@
 library(cbsots)
 
-source("functions/check_ts_table.R")
-
 id <- "83693NED"
 id <- "82522NED"
-#d <- "00372"
-#id <- "00372"
 
-ts_code_file <- "tscode/tscode_testje.rds"
+ts_code_file <- "tscode/tscode.rds"
+#edit_ts_code(ts_code_file)
 ts_code <- readRDS(ts_code_file)
 
 x <- get_ts(id, ts_code = ts_code, refresh = TRUE, download = FALSE, 
@@ -17,6 +14,3 @@ x <- get_ts(id, ts_code = ts_code, refresh = TRUE, download = FALSE,
 
 write_table_ts_xlsx(x, file.path("output", paste0("ts_", id, ".xlsx")), 
                      rowwise = TRUE)
-
-ret <- check_ts_table(x, id)
-print(ret$equal)
