ae66099fbca809c991b96251198f8c10ff397e2b
Rob van Harrevelt <rvanharrevelt@gmail.com>
2021-03-07 17:46:30 +0100

    Added additional test with errors.

--- cbsots/tests/testthat/test_get_ts_84328NED.R
+++ cbsots/tests/testthat/test_get_ts_84328NED.R
@@ -24,3 +24,27 @@ test_that(id, {
   check <- check_ts_table(result1, id, raw_cbs_dir = raw_cbs_dir)
   expect_true(check)
 })
+
+
+test_that(paste(id, "errors"), {
+  ts_code_err <- ts_code
+  ts_code_err$`84328NED`$codes$Topic[9, "Select"] <- TRUE
+  ts_code_err$`84328NED`$codes$Topic[9, "Code"] <- "xxx"
+  msg <-  paste0("Duplicate keys selected in timeseries coding for dimension",
+                " Topic in table 84328NED:\n",
+                "'KapitaalgoederenvoorraadBeginbalans_3', 'Afschrijvingen_6'",
+                ", 'KapitaalgoederenvoorraadEindbalans_1'\\.") 
+  expect_error(get_ts(id, ts_code_err, download = FALSE), msg)
+  
+  
+  ts_code_err <- ts_code
+  ts_code_err$`84328NED`$codes$Topic[7, "Code"] <- "wnd"
+  
+  expect_warning(result1 <- get_ts(id, ts_code_err, download = FALSE))
+  check <- check_ts_table(result1, id, raw_cbs_dir = raw_cbs_dir)
+  expect_true(check)
+  
+  ts_code_err$`84328NED`$codes$Topic[7, "Select"] <- TRUE
+  msg <- "Duplicate codes found for Topic:\nwnd\n."
+  expect_error(get_ts(id, ts_code_err, download = FALSE), msg)
+})
