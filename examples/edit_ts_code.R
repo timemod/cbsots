2ee8fe27b834eee309983dd99f578c63da78d85a
Harrevelt <rvh@cpb-rvh-l01p.cpb.nl>
2023-05-02 09:31:49 +0200

    Reverted recent changes in examples/edit_ts_code.R. And added a new file edit_ts_code_small.R

--- examples/edit_ts_code.R
+++ examples/edit_ts_code.R
@@ -2,12 +2,4 @@ library(cbsots)
 
 ts_code_file <- "tscode/tscode.rds"
 
-ts_code <- readRDS(ts_code_file)
-ts_code_small <- ts_code[1:3]
-attr(ts_code_small, "class") <- "ts_code"
-attr(ts_code_small, "package_version") <- "1.9.0"
-
-ts_code_file <- "tscode/tscode_small.rds"
-saveRDS(ts_code_small, ts_code_file)
-
 edit_ts_code(ts_code_file = ts_code_file, debug = TRUE)
